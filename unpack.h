8125e6376282a55ce33c86c791915012de862b6f
Jean-Luc Margot <jlm@epss.ucla.edu>
2002-05-28 01:19:36 +0000

    Added separate unpacking of RCP and LCP

--- unpack.h
+++ unpack.h
@@ -11,3 +11,7 @@ void unpack_pfs_4c4b(char *buf, float *rcp, float *lcp, int bufsize);
 void unpack_pfs_signedbytes(char *buf, float *outbuf, int bufsize);
 
 void unpack_pfs_signed16bits(char *buf, float *outbuf, int bufsize);
+
+
+void unpack_pfs_4c2b_lcp(char *buf, float *lcp, int bufsize);
+void unpack_pfs_4c2b_rcp(char *buf, float *rcp, int bufsize);
